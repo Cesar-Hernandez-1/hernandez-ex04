3ccd827bfc818a4fa7d9feed122fe482da77c1c3
Cesar-Hernandez-1 <cesarhernandez@Knights.ucf.edu>
2021-09-03 16:06:08 -0400

    Moved main class Solution04 to exercise04

---
+++ src/main/java/exercise04/Solution04.java
@@ -0,0 +1,36 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solution
+ *  Copyright 2021 Cesar Hernandez
+ */
+
+/*  Pseudocode:
+ *  Declare Scanner object "input" to receive input from user
+ *  Ask user to enter a noun and store input in "noun" string variable
+ *  Ask user to enter a verb and store input in "verb" string variable
+ *  Ask user to enter an adjective and store input in "adjective" string variable
+ *  Ask user to enter an adverb and store input in "adverb" string variable
+ *  Use printf() function to display and form a silly sentence using the variables stored with the user input
+ */
+
+package exercise04;
+import java.util.Scanner;
+public class Solution04 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a noun: ");
+        String noun = input.nextLine();
+
+        System.out.print("Enter a verb: ");
+        String verb = input.nextLine();
+
+        System.out.print("Enter a adjective: ");
+        String adjective = input.nextLine();
+
+        System.out.print("Enter a adverb: ");
+        String adverb = input.nextLine();
+
+        System.out.printf("Do you %s your %s %s %s? That's hilarious!", verb, adjective, noun, adverb);
+    }
+}
